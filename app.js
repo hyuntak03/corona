a6a781e8edb4f61ffb4c65b5f7a56f7a9089adfa
hyuntak03 <hyuntak03.lee@gmail.com>
2020-02-28 23:56:11 +0900

    modified

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express'); 
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) { 
    res.send('Hello World!'); 
 }); 
-app.listen(3000, function(){ 
-     console.log('App Listening on port 3000'); 
+
+app.listen(port, function () {
+    console.log('서버 실행중...');
 });
 
