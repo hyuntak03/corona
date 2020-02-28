92d4a329d49b7dc6414121bd115d3368212985b4
hyuntak03 <hyuntak03.lee@gmail.com>
2020-02-29 01:25:47 +0900

    modified

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var express = require('express'); 
 var app = express();
+var cheerio = require('cheerio')
+var request = require('request')
+var url = 'http://ncov.mohw.go.kr/index_main.jsp'
+var test_num = ''
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) { 
-   res.send('김나 승회 언제와 ㅅㅂ'); 
+    request(url, function(error, response, body){
+        var $ = cheerio.load(body)
+        $('.co_cur > ul').each(function(){
+            text = $(this).text()
+            test_text = text.toString()
+            //test_text = test_text.replace(/(^\s*)|(\s*$)/,"")
+            test_text = test_text.split('\n')
+            for(var i = 0; i<= 4;i++){
+                test_num += test_text[i].replace(/(^\s*)|(\s*$)/,"") + '\n'
+            }
+            res.send(test_num)
+            //console.log(test_num)
+        })
+        // console.log(num)
+    });
 }); 
 
 app.listen(port, function () {
