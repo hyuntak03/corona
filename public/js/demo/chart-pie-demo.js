55c685cd7f2b5ef24635a4d491789a31f10bc1ed
hyuntak03 <hyuntak03.lee@gmail.com>
2020-02-29 05:16:56 +0900

    modified

--- public/js/demo/chart-pie-demo.js
+++ public/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["확진자", "격리 해제", "사망"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [2337, 27, 13],
+      backgroundColor: ['#f6c23e', '#1cc88a', '#e74a3b'],
+      hoverBackgroundColor: ['#f6c23e', '#17a673', '#e74a3b'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
--- public/js/test/infected.js
+++ public/js/test/infected.js
@@ -2,13 +2,24 @@ var cheerio = require('cheerio')
 var request = require('request')
 var url = 'http://ncov.mohw.go.kr/index_main.jsp'
 
-request(url, function(error, response, body){
-    var $ = cheerio.load(body)
-    $('.co_cur > ul').each(function(){
-        text = $(this).text()
-        test_text = text.toString()
-        test_text = test_text.split('\n')
-        a = test_text[1].replace(/[^0-9]/g,"")
-        console.log(test_text[1].replace(/[^0-9]/g,""))
-    })
-});
+// function num(){
+//     request(url, function(error, response, body){
+//         var $ = cheerio.load(body)
+//         $('.co_cur > ul').each(function(){
+//             text = $(this).text()
+//             test_text = text.toString()
+//             test_text = test_text.split('\n')
+//             var a = test_text[1].replace(/[^0-9]/g,"")
+//             //document.write(a)
+//             //console.log(a)
+//             //a = test_text[1].replace(/[^0-9]/g,"")
+//             return a.toString();
+            
+//         })
+//     });
+// }
+
+function a(){
+    var b = 'test'
+    return b
+}
