d80cc7e7ebb6d188edb13973aff2c75899193374
hyuntak03 <hyuntak03.lee@gmail.com>
2020-03-01 01:43:31 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var cheerio = require('cheerio')
+var request = require('request')
+var url = 'http://ncov.mohw.go.kr/index_main.jsp'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/test', function(req,res,next){
+  request(url, function (error, response, body) {
+    var $ = cheerio.load(body)
+    $('.co_cur > ul').each(function () {
+      text = $(this).text()
+      test_text = text.toString()
+      test_text = test_text.split('\n')
+      var a = test_text[1].replace(/[^0-9]/g, "")
+      //document.write(a)
+      //console.log(a)
+      //a = test_text[1].replace(/[^0-9]/g,"")
+      res.render('test', {output: a})
+
+    })
+  });
+})
 
 module.exports = router;
