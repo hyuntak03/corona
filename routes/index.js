8088c065847052de7803c45c5d9117c1d6458728
hyuntak03 <hyuntak03.lee@gmail.com>
2020-03-01 01:59:17 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/test', function(req,res,next){
+router.get('/infected', function(req,res,next){
   request(url, function (error, response, body) {
     var $ = cheerio.load(body)
     $('.co_cur > ul').each(function () {
@@ -20,10 +20,42 @@ router.get('/test', function(req,res,next){
       //document.write(a)
       //console.log(a)
       //a = test_text[1].replace(/[^0-9]/g,"")
-      res.render('test', {output: a})
-
+      res.send(a)
     })
   });
 })
+
+router.get('/release', function(req,res,next){
+  request(url, function (error, response, body) {
+    var $ = cheerio.load(body)
+    $('.co_cur > ul').each(function () {
+      text = $(this).text()
+      test_text = text.toString()
+      test_text = test_text.split('\n')
+      var a = test_text[2].replace(/[^0-9]/g, "")
+      //document.write(a)
+      //console.log(a)
+      //a = test_text[1].replace(/[^0-9]/g,"")
+      res.send(a)
+    })
+  });
+})
+
+router.get('/die', function(req,res,next){
+  request(url, function (error, response, body) {
+    var $ = cheerio.load(body)
+    $('.co_cur > ul').each(function () {
+      text = $(this).text()
+      test_text = text.toString()
+      test_text = test_text.split('\n')
+      var a = test_text[3].replace(/[^0-9]/g, "")
+      //document.write(a)
+      //console.log(a)
+      //a = test_text[1].replace(/[^0-9]/g,"")
+      res.send(a)
+    })
+  });
+})
+
 
 module.exports = router;
