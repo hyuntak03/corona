310e99d477a4b6f681ffdceae5540af4670bf446
hyuntak03 <hyuntak03.lee@gmail.com>
2020-09-06 23:12:52 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,12 @@ router.get('/', function(req, res, next) {
 
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin:", "*");
-  res.header("Access-Control-Allow-Origin", "https://corona-data.herokuapp.com/infected"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //res.header("Access-Control-Allow-Origin", "https://corona-data.herokuapp.com/infected"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); 
+    return res.status(200).json({});
+  }
   next();
 });
 
