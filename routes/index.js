abf5374b78aea8cbe280695ac445721e7b855a00
hyuntak03 <hyuntak03.lee@gmail.com>
2020-09-06 23:04:50 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var cheerio = require('cheerio')
-var request = require('request')
-var url = 'http://ncov.mohw.go.kr/index_main.jsp'
+var cors = require('cors');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,11 +8,12 @@ router.get('/', function(req, res, next) {
 });
 
 router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin:", "*");
   res.header("Access-Control-Allow-Origin", "https://corona-data.herokuapp.com/infected"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Origin", "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+express.use(cors())
 
 module.exports = router;
