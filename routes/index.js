ff9fa6ab2f7b9a1bc5fd73d9d09368ee2b2c6b7d
hyuntak03 <hyuntak03.lee@gmail.com>
2020-09-06 23:10:22 +0900

    modified

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin:", "*");
+  res.header("Access-Control-Allow-Origin:", "*");
   res.header("Access-Control-Allow-Origin", "https://corona-data.herokuapp.com/infected"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
