93784436db0f8c78abda161ecbdceb67d963ffac
hyuntak03 <hyuntak03.lee@gmail.com>
2020-09-06 22:35:58 +0900

    modified

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var cors = require('cors')
+
 var app = express();
 
 // view engine setup
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
