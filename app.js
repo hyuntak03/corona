6e353204fd79ee03e1e1e2a6504b7c693618ef5b
hyuntak03 <hyuntak03.lee@gmail.com>
2021-07-09 00:52:27 +0900

    same-origin_policy_problem_sovled

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+const cors = require('cors')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(cors())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
